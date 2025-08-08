4d104603cbc8f4d5df01d1f1d5b7052e83c87296
Milan Bella <milan.bella>
2025-08-08 10:58:40 +0200

    m

--- f/ui.js
+++
@@ -1,8 +0,0 @@
-export function showMessage(message, type) {
-    const el = document.getElementById('message');
-    if (!el)
-        return;
-    el.textContent = message;
-    el.className = `message ${type}`.trim();
-}
-//# sourceMappingURL=ui.js.map
