4e3590ad180c35ab9361240eada6841ca002c02f
Milan Bella <milan.bella>
2025-09-24 20:14:03 +0200

    m

--- f/privacy.php
+++ f/privacy.php
@@ -17,11 +17,35 @@
     </header>
 
     <main class="disclaimer-container">
-    <h2>Privacy Policy</h2>
-    <p>We understand the importance of protecting your collected information. This Privacy Policy explains how we collect, use, and secure your data. By using our website, you agree to the practices described in this policy.</p>
+        <h2>Privacy Policy</h2>
+        <p>We understand the importance of protecting your collected information. This Privacy Policy explains how we collect, use, and secure your data. By using our website, you agree to the practices described in this policy.</p>
 
-    <h2>Information Collection</h2>
-    <p>We may collect the following user information: your given Roblox username(s), and your statistics on the website (offers completed, R$ earned, your referrals).</p>
+        <h2>Information Collection</h2>
+        <p>We may collect the following user information: your given Roblox username(s), and your statistics on the website (offers completed, R$ earned, your referrals).</p>
+
+        <h2>Use of Information</h2>
+        <p>The data we collect is exclusively used for statistical purposes. We do not disclose your information to third parties.</p>
+
+        <h2>Information Sharing</h2>
+        <p>We do not sell, rent, or share your information collected with third parties, except when necessary to provide the services you have requested—by providing only your Roblox username to which you are connected on the site.</p>
+
+        <h2>External Links</h2>
+        <p>Our site may contain links/integrations to other websites. We are not responsible for the privacy practices of these external sites.</p>
+
+        <h2>Policy Changes</h2>
+        <p>We may update this policy periodically. Please check regularly to stay informed of any changes.</p>
+
+        <h2>Request for Access to Your Personal Information / Deletion</h2>
+        <p>Since we do not collect sensitive information such as email addresses, it is not possible to delete your account registration on EarnObux.</p>
+
+        <h2>Compliance with Data Protection Regulations</h2>
+        <p>We ensure compliance with all relevant data protection regulations.</p>
+
+        <h2>Notification of Data Breach</h2>
+        <p>If data were to be compromised, we will communicate with our users through our Discord server.</p>
+
+        <h2>Contact Us</h2>
+        <p>For any questions or concerns regarding our privacy policy, feel free to contact us on Discord.</p>
     </main>
 
     <footer>
--- f/terms.php
+++ f/terms.php
@@ -17,14 +17,17 @@
     </header>
 
     <main class="disclaimer-container">
-    <h2>Terms of Service</h2>
-    <p>
-    Welcome to earnobux.com ! On this website, you can earn R$ for free by completing offers from our partners, who provide various ways to earn them, such as doing surveys, installing apps, watching videos, and other interesting methods. By accessing or using our website, you agree to be bound by the following Terms of Use. Please review them carefully before any use. If you do not agree to these terms, kindly refrain from using this site.
-    </p>
-    <h2>User Commitment</h2>
-    <p>
-    To start using our site and gain access to the page where you can get R$ for free, you need to link your Roblox account to our site by only providing your username. EarnObux will never ask you for your password, cookies, or any other information than your Roblox username. However, please note that to access certain offers on our website, it may be necessary to submit personal information to third parties, but we will never ask for a password. We disclaim any responsibility for the privacy practices and content of these third parties. Before completing any offer, it is recommended to carefully review their terms of use and privacy policies. Any bad-quality responses to surveys or attempts at fraud will result in an immediate and permanent ban from this site.
-    </p>
+        <h2>Terms of Service</h2>
+        <p>Welcome to earnobux.com ! On this website, you can earn R$ for free by completing offers from our partners, who provide various ways to earn them, such as doing surveys, installing apps, watching videos, and other interesting methods. By accessing or using our website, you agree to be bound by the following Terms of Use. Please review them carefully before any use. If you do not agree to these terms, kindly refrain from using this site.</p>
+
+        <h2>User Commitment</h2>
+        <p>To start using our site and gain access to the page where you can get R$ for free, you need to link your Roblox account to our site by only providing your username. EarnObux will never ask you for your password, cookies, or any other information than your Roblox username. However, please note that to access certain offers on our website, it may be necessary to submit personal information to third parties, but we will never ask for a password. We disclaim any responsibility for the privacy practices and content of these third parties. Before completing any offer, it is recommended to carefully review their terms of use and privacy policies. Any bad-quality responses to surveys or attempts at fraud will result in an immediate and permanent ban from this site.</p>
+
+        <h2>Disclaimer: No Guarantee of Offer Completion</h2>
+        <p>No assurance is given regarding the receipt of free R$ for completing each offer on our website. These offers are provided by third parties who have complete control over them. We do not endorse or certify the quality, accuracy, reliability, or relevance of these offers, but you can still contact us to make a claim. You acknowledge that completing an offer is done at your own risk, and we cannot be held responsible for any potential losses or damages resulting from this process.</p>
+
+        <h2>Intellectual Property Rights and Unauthorized Use</h2>
+        <p>All rights regarding our website and its content belong to us. Any reproduction, distribution, modification, display, performance, publication, transmission, or creation of derivative works from any part of our website or its content is prohibited without our express, prior written consent. Furthermore, the commercial use of our website or its content requires our prior written consent, to which you also agree to comply.</p>
     </main>
 
     <footer>
